8104060071fe8f39a137288f595dd3aef4b8f933
Roni <roni@Ronis-MacBook-Pro.local>
2018-05-20 22:45:37 +0700

    adding portfolio page

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio.hbs', {
+    pageTitle: 'Portfolio Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Error 404!'
